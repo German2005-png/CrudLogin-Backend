40d8677af3157b40db7a6a85eada58edaa88ef5d
German2005-png <73372926+German2005-png@users.noreply.github.com>
2023-08-27 23:32:42 +0000

    xdxdxd

--- database/app.database.js
+++ database/app.database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const AppMongodb = async() => {
     try {
-        await mongoose.connect('mongodb+srv://cloudtest.pxzr0pi.mongodb.net/GermanDev')
+        await mongoose.connect('mongodb+srv://GermanDev:<password>@cloudtest.pxzr0pi.mongodb.net/?retryWrites=true&w=majority')
         console.log('mongodb connecting!')
     } catch (error) {
         console.log('Err mongodb', error)
