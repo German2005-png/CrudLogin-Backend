9ad4d8edef99e8021ef4a215e0f81067781fd748
German2005-png <73372926+German2005-png@users.noreply.github.com>
2023-08-28 00:44:07 +0000

    jajaja

--- database/app.database.js
+++ database/app.database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const AppMongodb = async() => {
     try {
-        await mongoose.connect('mongodb+srv://GermanDev:AnahiSofia@cloudtest.pxzr0pi.mongodb.net/')
+        await mongoose.connect('mongodb+srv://GermanDev:AnahiSofia@cloudtest.pxzr0pi.mongodb.net/CrudTest')
         console.log('mongodb connecting!')
     } catch (error) {
         console.log('Err mongodb', error)
